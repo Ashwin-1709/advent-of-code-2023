a466fbc9e6abd96691cedbaaf60eb4da420cd972
Ashwin Pugalia <ashwinpugalia1@gmail.com>
2023-12-13 23:12:05 +0530

    add day 9 part 2

---
+++ Day 9/day_9_2.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+use std::{collections::HashMap, fs};
+
+const PARSE_ERROR: &str = "Parsing issue";
+const FILE_ERROR: &str = "File Operation Error";
+
+fn f(a: Vec<i32>) -> i32 {
+    let mut nxt: Vec<i32> = Vec::new();
+    let mut cnt0 = 0;
+    let front = *a.first().unwrap();
+    for i in 1..a.len() {
+        nxt.push(a[i] - a[i - 1]);
+        if a[i] == a[i - 1] {
+            cnt0 += 1;
+        }
+    }
+    if cnt0 == nxt.len() {
+        return front;
+    }
+    return front - f(nxt);
+}
+
+fn main() {
+    let input = fs::read_to_string("src/input.txt").expect(FILE_ERROR);
+    let arrays: Vec<&str> = input.lines().collect();
+    let mut ans: i32 = 0;
+    for array in arrays {
+        let cur: Vec<&str> = array.split_ascii_whitespace().collect();
+        let mut sequence: Vec<i32> = Vec::new();
+        for num in cur {
+            sequence.push(num.parse().expect(PARSE_ERROR));
+        }
+        ans += f(sequence);
+    }
+    println!("{}", ans);
+}
